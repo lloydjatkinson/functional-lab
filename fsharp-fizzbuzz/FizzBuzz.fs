c44b9d4b9175ef77414acba2aa9da9144c6508cd
Lloyd Atkinson <lloydjatkinson@gmail.com>
2018-07-30 01:04:23 +0100

    FizzBuzz

---
+++ fsharp-fizzbuzz/FizzBuzz.fs
@@ -0,0 +1,11 @@
+let fizzbuzz n =
+    match n % 3 = 0, n % 5 = 0 with
+        | true, false -> "Fizz"
+        | false, true -> "Buzz"
+        | true, true -> "FizzBuzz"
+        | false, false -> sprintf "%d" n
+
+let fizzbuzzSequence x y =
+    [x..y] |> Seq.map fizzbuzz
+
+fizzbuzzSequence 1 100 |> Seq.iter (printfn "%s")
