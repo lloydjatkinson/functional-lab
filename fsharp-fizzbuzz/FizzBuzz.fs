dca25b08e74ed47102e0910bfb5a0f166a34cff2
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2018-08-10 15:58:06 +0100

    Update FizzBuzz.fs

--- fsharp-fizzbuzz/FizzBuzz.fs
+++ fsharp-fizzbuzz/FizzBuzz.fs
@@ -1,9 +1,9 @@
 let fizzbuzz n =
     match n % 3 = 0, n % 5 = 0 with
-        | true, false -> "Fizz"
-        | false, true -> "Buzz"
-        | true, true -> "FizzBuzz"
-        | false, false -> sprintf "%d" n
+    | true, false -> "Fizz"
+    | false, true -> "Buzz"
+    | true, true -> "FizzBuzz"
+    | false, false -> sprintf "%d" n
 
 let fizzbuzzSequence x y =
     [x..y] |> Seq.map fizzbuzz
