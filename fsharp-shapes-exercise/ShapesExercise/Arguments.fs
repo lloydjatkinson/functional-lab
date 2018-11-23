e6793d580c5fa7c3c134cf60f5a9ad69e9a2142c
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2018-11-23 11:59:21 +0000

    Update Arguments.fs

--- fsharp-shapes-exercise/ShapesExercise/Arguments.fs
+++ fsharp-shapes-exercise/ShapesExercise/Arguments.fs
@@ -3,5 +3,4 @@
     type options =
         { [<Option('r', "read", Required = true, HelpText = "Input files.")>] files : seq<string>;
           [<Option(HelpText = "Prints all messages to standard output.")>] verbose : bool;
-          [<Option(Default = "русский", HelpText = "Content language.")>] language : string;
           [<Value(0, MetaName="offset", HelpText = "File offset.")>] offset : int64 option; }
